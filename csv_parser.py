85c9ba44fadcfee553c79bcc8241369981536b09
Evan Lin <evanslin@gmail.com>
2014-04-30 16:11:05 +0800

    It could correct character set to dump

--- csv_parser.py
+++ csv_parser.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import csv  
+import json
 
 '''
 Need reset system to utf8 encode way to if we want to decode big5 string
@@ -38,16 +39,45 @@ def force_decode(string, codecs=['utf8', 'big5']):
 #open big5 csv
 #http://data.gov.tw/node/5962
 print 'hotspotlist.csv'
-f = open('hotspotlist.csv', 'r')  
-for row in csv.reader(f):  
-    print force_decode(row[1])  
+csvfilename = 'hotspotlist.csv'
+jsonfilename = csvfilename.split('.')[0] + '.json'
+
+f = open(csvfilename, 'r')
+
+loop_count = 0
+output =[]
+#主管機關	地區	熱點名稱	地址	緯度	經度
+fieldnames = ("主管機關","地區","熱點名稱","地址", "緯度","經度")
+for each in csv.reader(f):  
+    #print force_decode(each[0])
+    #print force_decode(each[1])
+    row = {}
+    row['主管機關'] = each[0]
+    row['地區']  = each[1]
+    row['熱點名稱']  = each[2]
+    row['地址']   = each[3]
+    output.append(row)
+    print output 
+    loop_count+=1
+    #json.dump(row, jsonfile)
+    #jsonfile.write('\n')
+    if loop_count == 10:
+    	break
+out = json.dumps( [row for row in csv.reader(f) ], encoding='big5') 
+print "JSON parsed!"  
+jsonf = open(jsonfilename, 'w')  
+jsonf.write(out) 
 f.close()  
 
 #open utf8 csv
 #http://data.gov.tw/node/6794
 print 'car-utf.csv'
 f = open('car-utf.csv', 'r')  
+loop_count = 0
 for row in csv.reader(f):  
     print force_decode(row[1])  
+    loop_count += 1
+    if loop_count == 10:
+    	break
 f.close()  
 
